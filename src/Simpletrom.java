9e96fcf67a8451fa31fd2dd6e15201f3739593ac
artur732 <silveiraartur777@gmail.com>
2026-05-07 23:47:21 -0300

    Primeiro commit simpletron

---
+++ src/Simpletrom.java
@@ -0,0 +1,122 @@
+
+
+
+
+public class Simpletrom {
+
+
+    public void cases(String buffer) {
+        
+        char[] cadeia = new char[4];
+        cadeia=buffer.toCharArray();
+        
+        if(cadeia[0]=='0') {
+            
+            
+            
+        }
+        
+        if(cadeia[0]=='1') {
+            
+            if(cadeia[1]=='0') {
+                
+                //ler do teclado e armazenar
+                //em lugar da memória
+            }
+            
+            if(cadeia[1]=='1') {
+                
+                //exibir na tela a palavra
+                //armazenada em endereço
+                //expecífico
+            } else {
+                
+                //comando inválido
+            }
+        }
+        
+        if(cadeia[0]=='2') {
+
+            if(cadeia[1]=='0') {
+                
+                //copia de indereço beta
+                //para o acumulador
+            }
+            
+            if(cadeia[1]=='1') {
+                
+                //copia doacumulador
+                //para o endereço beta
+            } else {
+                
+                //comando inválido
+            }
+        }
+        
+        if(cadeia[0]=='3') {
+            
+            if(cadeia[1]=='0') {
+                
+                //soma o valor da memória
+                //ao acumulador
+            }
+            
+            if(cadeia[1]=='1') {
+                
+                //subtrai o valor da 
+                //memória do valor do 
+                //acumulador
+            } 
+            
+            if(cadeia[1]=='2') {
+             
+                //divide o valor do
+                //acumulador pelo 
+                //valor da memória
+            }
+            
+            if(cadeia[1]=='3') {
+                
+                //multiplica o valor
+                //do acumulador
+                //pelo valor da memória
+            }else {
+                
+                //comando inválido
+            }
+        }
+        
+        if(cadeia[0]=='4') {
+       
+            if(cadeia[1]=='0') {
+                
+                //JMP do assembly
+                //endereço específico
+            }
+            
+            if(cadeia[1]=='1') {
+                
+                //JMP se negativo
+                //endereço específico
+            } 
+            
+            if(cadeia[1]=='2') {
+             
+                //JMP if acumulador==0
+            }
+            
+            if(cadeia[1]=='3') {
+                
+                //fim de programa
+                
+            }else {
+                
+                //comando inválido
+            }
+        }
+        }
+        
+        
+    }
+    
+
